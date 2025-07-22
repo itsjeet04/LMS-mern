757f1d9a65c5a63c779606909369673482df1009
sirjanjeet singh <jeetsirjan@gmail.com>
2025-07-22 19:59:43 +0530

    course details page

--- LMS-MERN-client/src/pages/student/CourseDetails.jsx
+++ LMS-MERN-client/src/pages/student/CourseDetails.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
-
+import React, { useContext, useEffect } from 'react'
+import { AppContext } from '../../context/AppContext'
 function CourseDetails() {
+
+  const {id} = useParams()
+
+  const [courseData, setCourseData] = useState(null)  
+
+  const {allCourses} = useContext(AppContext)
+
+  const fetchCourseData = async () => {
+    const course = allCourses.find(course => course._id === id);
+    setCourseData(course);
+  }
+
+  useEffect(()=>{
+    fetchCourseData()
+  }, [id, allCourses])
+
+
   return (
-    <div>CourseDetails</div>
+    <>
+    <div>
+      {/* left column */}
+      <div>
+
+      </div>
+      {/* right column */}
+      <div>
+        
+      </div>
+    </div>
+    </>
   )
 }
 
