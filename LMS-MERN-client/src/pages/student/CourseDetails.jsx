a63d4d40540f5a8ca752ad65c518e16f3b13204c
sirjanjeet singh <jeetsirjan@gmail.com>
2025-07-25 19:50:32 +0530

    coursedetails

--- LMS-MERN-client/src/pages/student/CourseDetails.jsx
+++ LMS-MERN-client/src/pages/student/CourseDetails.jsx
@@ -1,5 +1,9 @@
 import React, { useContext, useEffect } from 'react'
 import { AppContext } from '../../context/AppContext'
+import Loading from '../../components/student/Loading'
+import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+
 function CourseDetails() {
 
   const {id} = useParams()
@@ -18,20 +22,31 @@ function CourseDetails() {
   }, [id, allCourses])
 
 
-  return (
-    <div>
-      <div>
-      {/* left column */}
-      <div>
+  return courseData ?(
+    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left '>
 
-      </div>
+      <div className='absolite top-0 left-0 w-fullh-secition-height -z-1 bg-gradient-to-b from-cyan-100/70'></div>
+
+
+
+      {/* left column */} 
+      <div>
+        <h1>
+          {courseData.courseTitle}
+        </h1>
+        <p>
+          {courseData.courseDescription}
+        </p>
+      <div>
+      
       {/* right column */}
+      </div>
       <div>
 
       </div>
     </div>
     </div>
-  )
+  ) : <Loading />
 }
 
 export default CourseDetails
