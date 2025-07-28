041a8318ddce2e3543778f958f11a81e9cc35a68
sirjanjeet singh <jeetsirjan@gmail.com>
2025-07-28 23:40:40 +0530

    setting context

--- LMS-MERN-client/src/context/AppContext.jsx
+++ LMS-MERN-client/src/context/AppContext.jsx
@@ -2,54 +2,77 @@ import { createContext } from "react";
 import { dummyCourses } from "../assets/assets";
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext();
 
-
-
-export  const AppContextProvider = (props) => {
-    // currency 
+export const AppContextProvider = (props) => {
     const currency = import.meta.env.VITE_CURRENCY || '$';
-
-    // 1- state to hold all courses
     const [allCourses, setAllCourses] = React.useState([]);
+
     const fetchAllCourses = async () => {
-        setAllCourses(dummyCourses)
-    }    
-    // 2- useEffect to fetch all courses when component mounts
+        setAllCourses(dummyCourses);
+    };
+
     useEffect(() => {
         fetchAllCourses();
     }, []);
 
-    // useNavigate hook
     const navigate = useNavigate();
-
     const [isEducator, setIsEducator] = React.useState(true);
 
-    // function to calc course rating
-    const calcCourseRating = (course) => 
-        {
-            if (!course.courseRatings || course.courseRatings.length === 0) return 0;
-            let totalRating = 0;
-            course.courseRatings.forEach((rating) => {
-                totalRating += rating.rating;
+    const calcCourseRating = (course) => {
+        if (!course.courseRatings || course.courseRatings.length === 0) return 0;
+        let totalRating = 0;
+        course.courseRatings.forEach((rating) => {
+            totalRating += rating.rating;
+        });
+        return totalRating / course.courseRatings.length;
+    };
+
+    const calcChapterTime = (chapter) => {
+        let time = 0;
+        chapter.chapterContent.forEach((lecture) => {
+            time += lecture.lectureDuration;
+        });
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m", "s"] });
+    };
+
+    const calcCourseTime = (course) => {
+        let time = 0;
+        course.courseContent.forEach((chapter) => {
+            chapter.chapterContent.forEach((lecture) => {
+                time += lecture.lectureDuration;
+            });
+        });
+        return humanizeDuration(time * 60 * 1000, { units: ["h", "m", "s"] });
+    };
+
+    const calcNumberOfLectures = (course) => {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if (Array.isArray(chapter.chapterContent)) {
+                totalLectures += chapter.chapterContent.length;
             }
-            );
-            return totalRating / course.courseRatings.length;
-    }
-
-
-    // calculalate course length time 
-    
+        });
+        return totalLectures;
+    };
 
     const value = {
-        currency,allCourses,navigate,calcCourseRating,isEducator,setIsEducator
-    }
+        currency,
+        allCourses,
+        navigate,
+        calcCourseRating,
+        isEducator,
+        setIsEducator,
+        calcNumberOfLectures,
+        calcCourseTime,
+        calcChapterTime
+    };
 
     return (
         <AppContext.Provider value={value}>
-        {props.children}
+            {props.children}
         </AppContext.Provider>
     );
-}
+};
