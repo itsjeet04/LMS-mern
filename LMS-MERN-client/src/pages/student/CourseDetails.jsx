659be6ab999079c1422507f8756d3d3f3acf908f
sirjanjeet singh <jeetsirjan@gmail.com>
2025-08-03 20:50:49 +0530

    preview

--- LMS-MERN-client/src/pages/student/CourseDetails.jsx
+++ LMS-MERN-client/src/pages/student/CourseDetails.jsx
@@ -5,6 +5,7 @@ import { assets } from '../../assets/assets';
 import Loading from '../../components/student/Loading';
 import humanizeDuration from 'humanize-duration';
 import Footer from '../../components/student/Footer';
+import YouTube from 'react-youtube'
 
 function CourseDetails() {
   const { id } = useParams();
@@ -14,6 +15,7 @@ function CourseDetails() {
   const [openSections, setOpenSections] = useState({});
   const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
   const [isEnrolled , setIsEnrolled]  = useState(false)
+  const [playerData,setPlayerData] = useState(null)
 
   useEffect(() => {
     // Finds the course from the context based on the URL parameter.
@@ -143,7 +145,9 @@ function CourseDetails() {
                                 <p className="font-medium text-gray-700">{lecture.lectureTitle}</p>
                                 <div className="flex items-center gap-3 text-sm text-gray-500 mt-1">
                                   {lecture.isPreviewFree && (
-                                    <p className="text-green-600 font-semibold flex items-center gap-1">
+                                    <p  onClick={()=>setPlayerData({
+                                      videoId : lecture.lectureUrl.split('/').pop()
+                                    })}  className="text-green-600 font-semibold flex items-center gap-1">
                                       <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
                                       Preview
                                     </p>
@@ -180,12 +184,16 @@ function CourseDetails() {
 
           {/* --- Right Column: Purchase Card --- */}
           <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-sm">
+            {
+              playerData ?      
+              <YouTube videoId={playerData.videoId} opts={{playerVars : {autoplay : 1}}} iframeClassName='w-full'/>
+              : 
             <img
               src={courseData.courseThumbnail}
               alt="course"
               className="w-full h-48 object-cover"
             />
-
+            }
             <div className="p-5 flex items-center gap-2 bg-gray-50">
               <img
                 src={assets.time_left_clock_icon}
