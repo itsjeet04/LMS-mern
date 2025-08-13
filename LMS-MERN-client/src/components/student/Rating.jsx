3e96e19946c4d6f22b152287cc8da01be50a0a98
sirjanjeet singh <jeetsirjan@gmail.com>
2025-08-13 15:44:02 +0530

    ratings section

--- LMS-MERN-client/src/components/student/Rating.jsx
+++ LMS-MERN-client/src/components/student/Rating.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-function Rating() {
+function Rating({initialRating , onRate}) {
+
+  const [rating, setRating] = React.useState(initialRating || 0);
+
+  const handleRatingChange = (value) => {
+    setRating(value);
+    if (onRate) {
+      onRate(value);
+    }
+  }
+
+  useEffect(() => {
+    if (initialRating) {
+      setRating(initialRating);
+    }
+  }, [initialRating]);
+
   return (
     <div>
-    {Array.from({length:5} , 
+    {Array.from({length:5} , (_ , index)=>{
+      const starValue = index + 1;
+      return (
+        <span key={index} className={`text-xl sm:text-2xl md:text-3xl cursor-pointer transition-color ${starValue <= rating ? 'text-yellow-500' : 'text-gray-300'}`} 
+          onClick={() => handleRatingChange(starValue)}>
+          &#9733; {/* Unicode for filled star */}
+        </span>)
+    }
     )}
     </div>
   )
