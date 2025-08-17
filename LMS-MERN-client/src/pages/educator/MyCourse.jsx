c9c246fbffba2562f469cd961e91c1922e00b149
sirjanjeet singh <jeetsirjan@gmail.com>
2025-08-17 23:59:45 +0530

    ok

--- LMS-MERN-client/src/pages/educator/MyCourse.jsx
+++ LMS-MERN-client/src/pages/educator/MyCourse.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useContext, useState}from 'react';
+import { AppContext } from '../../context/AppContext'
+import Loading from '../../components/student/Loading';
 
 function MyCourse() {
-  return (
-    <div>MyCourse</div>
-  )
+
+  const {currency , allcourses} = useContext(AppContext)
+  
+  const [courses, setCourses] = useState();
+  
+  const fetchEducatorCourses = async () => {
+    setCourses(allcourses)
+  }
+
+  useState(()=> {
+    fetchEducatorCourses();
+  }, []);
+
+
+  return courses ?  (
+    <div>
+      <div>
+        <h2>
+          My Courses
+        </h2>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>
+                All Courses
+              </td>
+              <td>
+                Earnings
+              </td>
+              <td>
+                Students
+              </td>
+              <td>
+               Published on 
+               {/* ok/}
+              </td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+
+
+     
+    </div>
+  ) : <Loading/>
 }
 
 export default MyCourse
