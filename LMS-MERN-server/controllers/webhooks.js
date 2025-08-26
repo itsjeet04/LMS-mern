4283b803b13a2534225979d2733baed25b059e50
sirjanjeet singh <jeetsirjan@gmail.com>
2025-08-27 00:48:34 +0530

    versel json added

---
+++ LMS-MERN-server/controllers/webhooks.js
@@ -0,0 +1,59 @@
+import { Webhook } from "svix";
+// webhook is a middleware function that verifies the incoming events
+// A webhook is a way for one application to notify another in real time.
+// Webhook = event notification via HTTP.
+
+import User from "../models/user.model.js";
+
+
+//api controller function to manage clerk user with database
+export const clerkWebhooks = async (req, res) => {
+    try {
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+         whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        });
+        const { type, data } = req.body;
+
+        switch (type) {
+            case "user.created": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url
+                }
+                await User.create(userData)
+                res.json({ message: "User created successfully" });
+                break;
+            }
+
+            case "user.deleted": {
+                await User.findByIdAndDelete(data.id);
+                res.json({ message: "User deleted successfully" })
+                break;
+            }
+
+            case "user.updated": {
+                const userData = {
+                    email: data.email_addresses[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url
+                }
+                await User.findByIdAndUpdate(data.id, userData)
+                res.json({ message: "User updated successfully" })
+                break;
+            }
+
+            default:
+                break;
+        }
+
+    } catch (error) {
+        console.error("Error processing webhook:", error);
+        res.status(400).json({ message: "Error processing webhook" });
+    }
+}
--- LMS-MERN-server/server.js
+++ LMS-MERN-server/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { connectDB } from './configs/mongoDB.js';
+import { clerkWebhooks } from './controllers/webHooks.js';
 
 // express init
 const app = express();
+
+// Database connection
+await connectDB();
 
 // Middleware
 app.use(cors());
@@ -11,8 +16,9 @@ app.use(cors());
 // Routes
 app.get('/', (req, res) => {
     res.send('API is running...')
-}
+    }
 )
+app.post('/clerk' ,express.json(), clerkWebhooks )
 
 // Port
 const PORT = process.env.PORT || 5000;
