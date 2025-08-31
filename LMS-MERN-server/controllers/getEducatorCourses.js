25fca0a1fbc93b690e269acc9858515f1d62ad39
sirjanjeet singh <jeetsirjan@gmail.com>
2025-08-31 19:02:16 +0530

    Add getEducatorCourses controller and update educatorRoutes to include course retrieval

---
+++ LMS-MERN-server/controllers/getEducatorCourses.js
@@ -0,0 +1,15 @@
+import Course from "../models/course.model.js"
+
+
+export const getEducatorCourses = async(req,res) => {
+    const educator = req.auth.userId 
+    console.log(educator)
+
+    try {
+        const courses = await Course.find({educator})
+        res.json({success : true , courses})
+    } catch (error) {
+        console.error("error in getting course : " ,error)
+        res.json({success : false , message : error.message})
+    }
+}
--- LMS-MERN-server/routes/educatorRoutes.js
+++ LMS-MERN-server/routes/educatorRoutes.js
@@ -3,6 +3,7 @@ import { updateRoleToEducator } from '../controllers/educatorController.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { protectEducator } from '../middlewares/auth.middleware.js';
 import { addNewCourse } from '../controllers/addNewCourse.js';
+import { getEducatorCourses } from '../controllers/getEducatorCourses.js';
 
 
 const educatorRouter = express.Router();
@@ -10,5 +11,6 @@ const educatorRouter = express.Router();
 // add educator role to user 
 educatorRouter.post('/update-role', updateRoleToEducator);
 educatorRouter.get('/add-course', upload.single('thumbnail') , protectEducator , addNewCourse );
+educatorRouter.get('/courses', protectEducator , getEducatorCourses );
 
 export default educatorRouter;
